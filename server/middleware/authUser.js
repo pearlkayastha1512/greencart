d2602709e2bc2c5365c0e76b7899b581ef0833a9
pearl <pearl.gkp@gmail.com>
2025-04-19 04:18:06 +0530

    First commit

---
+++ server/middleware/authUser.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+
+
+//next-execute the controller function
+//get the cookies from request ,whenever we will request any api endpoint we will get cookie and that cookie contains token
+const authUser = async (req,res,next)=>{
+    const {token} = req.cookies;
+    if(!token){
+        return res.json({success:false,message:'Not Authorized'});
+    }
+    try{
+        const tokenDecode = jwt.verify(token,process.env.JWT_SECRET)
+        if (tokenDecode.id) {
+            req.user = tokenDecode.id; // ✅ store in req.user instead of req.body
+            next();
+        } else {
+            return res.status(401).json({ success: false, message: 'Invalid Token' });
+        }
+        next();
+    }catch(error){
+        console.log(error.message);
+        res.json({success:false,message:error.message});
+    }
+
+}
+export default authUser;
