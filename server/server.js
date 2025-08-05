f08ea7f5635987861daa973d8b2b4f79b8fbdd77
pearlkayastha1512 <167806394+pearlkayastha1512@users.noreply.github.com>
2025-08-05 10:54:19 +0530

    port changes

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDb()
 await connectCloudinary()
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','http://localhost:5174']
 app.post('/stripe',express.raw({type: 'application/json'}),stripeWebhooks)
 
 //Middleware configuration
